f175ae72ccc00b49a9d214264dc334b9c22a57fc
Mostafejur Rahman <106027543+mostafejur21@users.noreply.github.com>
2024-10-22 17:08:14 +0600

    Refactor MemesController and HomePage
    - Added a new list variable `_filteredMemes` to store filtered memes.
    - Implemented the `searchMemes` method in MemesController to filter memes based on a query.
    - Updated the HomePage UI to include a search bar and display filtered memes.

--- lib/module/controller/memes_controller.dart
+++ lib/module/controller/memes_controller.dart
@@ -9,6 +9,8 @@ class MemesController extends GetxController {
   bool _isLoading = false;
   bool get isLoading => _isLoading;
   List<Meme> _memes = [];
+  List<Meme> _filteredMemes = [];
+  List<Meme> get filteredMemes => _filteredMemes;
   List<Meme> get memes => _memes;
 
   @override
@@ -21,15 +23,25 @@ class MemesController extends GetxController {
     _isLoading = true;
     update();
     final response = await http.get(Uri.parse(Urls.GET_MEMES));
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       _isLoading = false;
       _memes = MemeResponse.fromJson(jsonDecode(response.body)).memes;
+      _filteredMemes = _memes;
       update();
       return true;
-    }else{
+    } else {
       _isLoading = false;
       update();
       return false;
     }
+  }
+
+  void searchMemes(String query) {
+    if (query.isEmpty) {
+      _filteredMemes = _memes;
+    } else {
+      _filteredMemes = _memes.where((meme) => meme.name.toLowerCase().contains(query.toLowerCase())).toList();
+    }
+    update();
   }
 }
--- lib/module/presentation/home_page.dart
+++ lib/module/presentation/home_page.dart
@@ -9,35 +9,72 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Memes")),
-        body: GetBuilder<MemesController>(builder: (controller) {
-          return ListView.separated(
-            separatorBuilder: (context, index) => const Divider(),
-            itemCount: controller.memes.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: (){
-                  Get.to(()=> MemeDetailsPage(meme: controller.memes[index],));
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.network(
-                      controller.memes[index].url,
-                      height: 200,
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      controller.memes[index].name,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                  ],
+      appBar: AppBar(title: const Text("Memes")),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (value) {
+                Get.find<MemesController>().searchMemes(value);
+              },
+              decoration: const InputDecoration(
+                hintText: "Search Memes",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+            ),
+          ),
+          GetBuilder<MemesController>(
+            builder: (controller) {
+              if (controller.isLoading) {
+                // Show a loading indicator while the data is being fetched
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              if (controller.filteredMemes.isEmpty) {
+                // Show a message if no memes are available
+                return const Center(
+                  child: Text("No memes available"),
+                );
+              }
+
+              return Expanded(
+                child: ListView.separated(
+                  separatorBuilder: (context, index) => const Divider(),
+                  itemCount: controller.filteredMemes.length,
+                  itemBuilder: (context, index) {
+                    final meme = controller.filteredMemes[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Get.to(() => MemeDetailsPage(meme: meme));
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.network(
+                            meme.url,
+                            height: 200,
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            meme.name,
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
               );
             },
-          );
-        }));
+          ),
+        ],
+      ),
+    );
   }
 }
